a16ee1a005a618f8f63489f5197b5590790d733b
pollyanazleticia-spec <pollyanazleticia@gmail.com>
2025-10-24 20:33:19 +0000

    Adiciona arquivo atividade.dart e pasta tarefas

---
+++ tarefas/obj/tarefas.cs
@@ -0,0 +1,9 @@
+piblic class Tarefa
+{
+    public id int {get; set;}
+    public nome string {get; set;}
+    public descricao string {get; set;}
+    public dataCriacao Datetime {get; set;}
+    public status int {get; set;}
+    public dataExecucao datetime {get; set;}
+}
